1573b632fcd9f447c9e98916836fbd672633d58c
Przemysław Szelenberger <Przemyslaw.Szelenberger@ingbank.pl>
2016-03-22 14:24:55 +0100

    refaktoring

--- js/toggle-passwords/js/main.js
+++ js/toggle-passwords/js/main.js
@@ -1,19 +1,26 @@
 (function($) {
   "use strict";
 
-  $( function() {
-
-    var $form = $(".js-form"),
-      $submit = $(".js-submit"),
+  $(function() {
+    var
+      $form = $(".js-form"),
       $field = $(".js-password"),
-      _template = "<a href='#' class='js-password-toggle form__toggle'><i class='fa  fa-eye-slash fa-eye form__toggle-button'></a>",
+
+      _template = [
+        "<a href='#' class='js-password-toggle form__toggle'>",
+          "<i class='fa  fa-eye-slash fa-eye form__toggle-button'>",
+        "</a>"
+      ].join(""),
+
+      $submit,
       $toggle;
 
     $field.after( _template );
-    $toggle = $(".js-password-toggle");
 
-    $toggle.on("click", function toggleFieldType (e) {
+    function toggleFieldType (e) {
       var
+        eyeSlash = "fa-eye-slash",
+        eye = "fa-eye",
         $target;
 
       if (e) {
@@ -24,30 +31,35 @@
       if ($field.attr("type") === "password") {
         $field.attr("type","text");
         $target
-          .removeClass("fa-eye-slash")
-          .addClass("fa-eye");
+          .removeClass(eyeSlash)
+          .addClass(eye);
       }
 
       else {
         $field.attr("type","password");
         $target
-          .removeClass("fa-eye")
-          .addClass("fa-eye-slash");
+          .removeClass(eye)
+          .addClass(eyeSlash);
       }
+    }
 
-    });
-
-    // Set the form field back to a regular password element
-    $submit.on( "click", function setToRegularPasswordElement (e) {
+    function setToRegularPasswordElement (e) {
       if (e) {
         e.preventDefault();
       }
 
       $field.attr("type","password");
-    });
+      $submit.submit();
+    }
+
+    $toggle = $(".js-password-toggle")
+      .on("click", toggleFieldType);
+
+    $submit = $(".js-submit")
+      .on("click", setToRegularPasswordElement);
 
     // Stop the form submitting so that we can test it (remove this in a real form):
-    $form.submit( function stopSubmittingForm (e) {
+    $form.submit(function stopSubmittingForm (e) {
       if (e) {
         e.preventDefault();
       }
